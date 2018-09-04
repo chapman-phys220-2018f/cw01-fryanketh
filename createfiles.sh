fabe9906c726bf5a0a6dbcb3d2c8598639e5c7d1
gwynethcasey <42781436+gwynethcasey@users.noreply.github.com>
2018-09-03 19:58:34 -0700

    Add files via upload

--- createfiles.sh
+++ createfiles.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 mkdir tempfiles
-
-numberoffiles=100
+numfiles=100
 
 cd tempfiles
 
-for i in $(seq $numberoffiles); do echo > file$i.tmp; done
+for i in $(seq $numfiles); do echo > file$i.tmp; done
 
-for i in $(seq $numberoffiles); do echo "Temporary file $i" >> file$i.tmp; done
+for i in $(seq $numfiles); do echo "Temporary file $i" >> file$i.tmp; done
 
 exit 0
